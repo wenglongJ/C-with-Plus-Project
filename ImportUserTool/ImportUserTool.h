c094e7db734cea065d7eb43f81a58f225007fb5a
jiangwl <jiangwl@ruiji.com>
2023-01-17 10:27:32 +0800

    1、导入工具

---
+++ ImportUserTool/ImportUserTool.h
@@ -0,0 +1,32 @@
+
+// ImportUserTool.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CImportUserToolApp:
+// See ImportUserTool.cpp for the implementation of this class
+//
+
+class CImportUserToolApp : public CWinApp
+{
+public:
+	CImportUserToolApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CImportUserToolApp theApp;
