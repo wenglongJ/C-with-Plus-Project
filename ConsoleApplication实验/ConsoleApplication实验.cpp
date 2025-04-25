d6a1ab8bf9af5fa24c406cbce40914cb3b647003
jwl-sky <59241172+jwl-sky@users.noreply.github.com>
2025-04-25 23:07:47 +0800

    modify document

---
+++ ConsoleApplication实验/ConsoleApplication实验.cpp
@@ -0,0 +1,13 @@
+﻿// ConsoleApplication实验.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    int i=10;
+    std::cout << "dsad" << std::endl;
+    std::cout << "Hello World!\n";
+}
+
